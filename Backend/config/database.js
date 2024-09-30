4d5c3b7ca98d6a9192b8a52a6b2fff646d97aba0
Abhinandan Daksh <Abhinandandaksh@gmail.com>
2024-09-30 23:41:54 +0530

    database

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
         console.log('Database connected');
     }).catch((error)=>{
         console.log(error);
