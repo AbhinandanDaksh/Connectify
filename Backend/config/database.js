669e4c16f54ba8a13626252e926e32783b37fa7d
Abhinandan Daksh <Abhinandandaksh@gmail.com>
2024-10-06 17:09:28 +0530

    backend

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const database = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
     }).then(() => {
         console.log('Database connected');
     }).catch((error) => {
