2b2c1282069d261b386b0f60db04fb7ff85264dc
Abhinandan Daksh <Abhinandandaksh@gmail.com>
2024-10-07 17:37:51 +0530

    frontend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://connectify-backend-72p4.onrender.com/"
+export const BASE_URL="https://connectify-backend-72p4.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
