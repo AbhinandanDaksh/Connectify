36c6bc6ed31b7c75bda6baa94d988d03fc0377ae
Abhinandan Daksh <Abhinandandaksh@gmail.com>
2024-10-07 17:30:09 +0530

    Backend url set

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://connectify-xi-plum.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
